432aa69d06218e6b732a7b82e263c8826d70faf9
Aemie <aemie.j@hotmail.co.uk>
2020-09-14 20:36:48 +0530

    add basic setup of chrome extension

---
+++ scripts/extension/background.js
@@ -0,0 +1,4 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.browserAction.onClicked.addListener(tab => {    
+    })
+});
---
+++ scripts/extension/popup.js
@@ -0,0 +1,5 @@
+let submitButton = document.querySelector("div.page label input.submit");
+submitButton.addEventListener("click", () => {
+  let userName = document.querySelector("div.page label input.userName").value;
+  
+});
